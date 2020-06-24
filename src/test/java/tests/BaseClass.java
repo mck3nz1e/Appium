506aaa2fb9df9a4e4ec49dbddd640b761dae27c1
Scott McKenzie <scottmckenzie09@googlemail.com>
2020-06-24 23:30:18 +0100

    Added Extent Reports to BaseTest

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -3,14 +3,22 @@ package tests;
 import java.net.URL;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.google.common.collect.ImmutableMap;
 
+import ScreenShots.screenCaptureBase64;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 
@@ -18,19 +26,26 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class BaseClass {
 
-	public static ExtentHtmlReporter reporter;
-	public static ExtentReports extent;
-	public static ExtentTest logger;
 	AppiumDriver<MobileElement> driver;
+	ExtentSparkReporter htmlReporter;
+	ExtentReports extent;
+	ExtentTest test;
+
+	@BeforeSuite
+	public void reportSetup() {
+
+		// start reporters
+		htmlReporter = new ExtentSparkReporter("extent.html");
+
+		// create ExtentReports and attach reporter(s)
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+	}
 
 	@BeforeTest
 	public void setup() {
 
 		try {
-
-			reporter = new ExtentHtmlReporter("./Reports/ExtentReport1.html");
-			extent = new ExtentReports();
-			extent.attachReporter(reporter);
 
 			DesiredCapabilities caps = new DesiredCapabilities();
 			caps.setCapability("appium:chromeOptions", ImmutableMap.of("w3c", false));
@@ -61,4 +76,35 @@ public class BaseClass {
 		driver.quit();
 	}
 
+	@AfterMethod
+	public void closeBrowser(ITestResult result) throws Exception {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
+
+			test.info(MarkupHelper.createLabel(result.getName() + "Test Case Failed", ExtentColor.RED));
+			Thread.sleep(1000);
+			String temp = screenCaptureBase64.CaptureScreenShot64(driver);
+			test.fail(result.getThrowable().getMessage(),
+					MediaEntityBuilder.createScreenCaptureFromBase64String(temp).build());
+
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+
+			test.info(MarkupHelper.createLabel(result.getName() + "Test Case Passed", ExtentColor.GREEN));
+		}
+
+		else {
+
+			test.skip(MarkupHelper.createLabel(result.getName() + "Test Case Passed", ExtentColor.ORANGE));
+			test.skip(result.getThrowable());
+		}
+
+		extent.flush();
+		driver.close();
+	}
+
+	@AfterSuite
+	public void reportTeardown() {
+
+		extent.flush();
+	}
 }
--- src/test/java/tests/WebAppTests.java
+++ src/test/java/tests/WebAppTests.java
@@ -3,42 +3,58 @@ package tests;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.MediaEntityBuilder;
-
-import ScreenShots.screenCaptureBase64;
+import com.aventstack.extentreports.Status;
 import pageObjects.practiceForm;
 
 public class WebAppTests extends BaseClass {
 
 	@Test
 	public void happyPath() throws Exception {
-		
+
+		test = extent.createTest("happyPath", "This is a happy path end to end scenario.");
+		test.log(Status.INFO, "Happy Path Test 1 Started.");
+
 		driver.get("http://www.demoqa.com/automation-practice-form");
+		test.log(Status.PASS, "Navigate to the practice form");
 
 		practiceForm objects = new practiceForm(driver);
-
+		
 		objects.givenName("Scott");
+		test.log(Status.PASS, "Entered first name.");
+
 		objects.surname("McKenzie");
+		test.log(Status.PASS, "Entered surnname.");
+
 		objects.email("testing@test.co.uk");
+		test.log(Status.PASS, "Entered email address.");
+
 		driver.findElement(By.id("userName-label")).click();
+		
 		objects.gender_Male();
+		test.log(Status.PASS, "Selected gender.");
+
 		objects.contactNumber("07123456789");
+		test.log(Status.PASS, "Entered mobile number.");
+
 		driver.findElement(By.id("userName-label")).click();
 
 		objects.hobbies_sport();
+		test.log(Status.PASS, "Selected sport as a hobby.");
+
 		objects.address("21 Test Street \nTest Town");
+		test.log(Status.PASS, "Entered address.");
+
 		driver.findElement(By.id("userName-label")).click();
 
 		objects.submitForm();
+		test.log(Status.PASS, "Submitted form.");
 
 		String actualText = objects.modalHeader();
-		String expectedText = "Thanks for submitting the formz";
-		
-		String temp = screenCaptureBase64.CaptureScreenShot64(driver);
-		MediaEntityBuilder.createScreenCaptureFromBase64String(temp).build();
+		String expectedText = "Thanks for submitting the form";
 
 		Assert.assertEquals(actualText, expectedText);
+		test.log(Status.PASS, "Asserted the modal title.");
+
+		test.log(Status.INFO, "Test complete");
 	}
-	
 }
